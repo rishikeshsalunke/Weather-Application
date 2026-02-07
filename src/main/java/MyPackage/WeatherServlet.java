c30b374bd32af97cf067af14a0aedf85f879b7b4
Rishikesh Salunke <rishisalunke14@gmail.com>
2026-02-08 02:31:45 +0530

    done

--- src/main/java/MyPackage/WeatherServlet.java
+++ src/main/java/MyPackage/WeatherServlet.java
@@ -20,7 +20,7 @@ import com.google.gson.JsonObject;
  * Servlet implementation class WeatherServlet
  */
 public class WeatherServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L; 
 
 	/**
 	 * @see HttpServlet#HttpServlet()
